e671b66cff6031bd33e440009d4a83873d0f9fa1
yuyaoli <yuyaoli2002@gmail.com>
2025-08-05 14:41:16 +0800

    Remove __pycache__ from repository and update .gitignore

--- kag/reflection/__init__.py
+++
@@ -1,8 +0,0 @@
-# kag/reflection/__init__.py
-
-from .reflection_manager import ReflectionManager
-
-__all__ = [
-    "ReflectionManager",
-]
-
--- kag/storage/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-存储模块
-"""
-
-from .graph_store import GraphStore
-from .vector_store import VectorStore
-
-__all__ = ["GraphStore", "VectorStore"]
-
