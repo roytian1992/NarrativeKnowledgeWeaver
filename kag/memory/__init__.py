e1b69071934b3bdaee815c5803926505aa006c5c
yuyaoli <yuyaoli2002@gmail.com>
2025-08-05 15:07:16 +0800

    clean

--- kag/memory/__init__.py
+++ kag/memory/__init__.py
@@ -1,13 +1,11 @@
 # kag/memory/__init__.py
 
 from .base_memory import BaseMemory
-from .buffer_memory import BufferMemory
 from .vector_memory import VectorMemory
 from .summary_memory import SummaryMemory
 
 __all__ = [
     "BaseMemory",
-    "BufferMemory",
     "VectorMemory",
     "SummaryMemory",
 ]
